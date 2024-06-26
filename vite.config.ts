4d1a6880b674e746cb23777082bbc25547f307b2
shimaoka hitoshi <shimaoka1203@gmail.com>
2024-06-26 18:27:41 +0900

    実行するポートを3000番に変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+    // serverオプションを追加
+    server: {
+      // port番号を指定
+      port: 3000,
+    }
 })
