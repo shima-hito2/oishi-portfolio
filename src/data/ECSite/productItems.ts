890062f4e0d5db67eac9e613debc1613355316da
puruyahha1ha <puruyahha1ha@gmail.com>
2024-08-28 23:53:55 +0900

    ECサイト パスの修正。

--- src/data/ECSite/productItems.ts
+++ src/data/ECSite/productItems.ts
@@ -4,21 +4,21 @@ export const productItems = [
 	{
 		title: '商品1',
 		price: '1000円',
-		image: getImage('ECsite/image11.png'),
+		image: getImage('ECSite/image11.png'),
 	},
 	{
 		title: '商品2',
 		price: '2000円',
-		image: getImage('ECsite/image11.png'),
+		image: getImage('ECSite/image11.png'),
 	},
 	{
 		title: '商品3',
 		price: '3000円',
-		image: getImage('ECsite/image11.png'),
+		image: getImage('ECSite/image11.png'),
 	},
 	{
 		title: '商品4',
 		price: '4000円',
-		image: getImage('ECsite/image11.png'),
+		image: getImage('ECSite/image11.png'),
 	},
 ];
